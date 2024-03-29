aa249d13172637b3851705ea1c50fbfbc6c92d46
Louis-ThomasNolet <louis.thomas.nolet@icloud.com>
2024-03-28 20:07:30 -0400

    fix 4

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/appweb-exer16-v2/",
+  plugins: [vue()],
+})
